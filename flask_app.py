aaeaef52515a44316b0ddb1a39e71665993ea766
doc4science <30442918+doc4science@users.noreply.github.com>
2023-10-25 23:19:04 -0500

    Add check to ensure number of courses is a number

--- flask_app.py
+++ flask_app.py
@@ -17,33 +17,62 @@ def adder_page():
         include_input = None
         exclude_input = None
         course_input = request.form["course_input"]
-        combination_input = request.form["combination_input"]
+        try:
+            combination_input = float(request.form["combination_input"])
+        except:
+             errors += "<p>Please enter a number in Number of Courses</p>\n".format(request.form["combination_input"])
         include_input = request.form["include_input"]
         exclude_input = request.form["exclude_input"]
 
-        returnlist = solution(course_input, combination_input, include_input, exclude_input)
-        allcomblength = returnlist[0]
-        comb_no_repeat = returnlist[1]
-        comb_clusters = returnlist[2]
+        if errors == "":
+            returnlist = solution(course_input, combination_input, include_input, exclude_input)
+            
+            allcomblength = returnlist[0]
+            comb_no_repeat = returnlist[1]
+            comb_clusters = returnlist[2]
 
-        formattedblocks = ""
-        for i in range(len(comb_no_repeat)):
-            temp = '''<div class="blocks">
-                            <p class="block-title">'''
-            temp += " ".join(comb_no_repeat[i])
-            temp += '''</p>
-                            <ul>'''
-            for j, course in enumerate(comb_clusters[i]):
-                temp += "<li>"
-                temp += " ".join(course)
-     ##  FOR TIMES       # temp += "</li></li>"
-     ##  FOR TIMES       # temp += " ".join(print_output(course))
-                temp += "</li>"
-            temp += '''</ul>
-                        </div>'''
-            formattedblocks += temp
+            formattedblocks = ""
+            for i in range(len(comb_no_repeat)):
+                temp = '''<div class="blocks">
+                                <p class="block-title">'''
+                temp += " ".join(comb_no_repeat[i])
+                temp += '''</p>
+                                <ul>'''
+                for j, course in enumerate(comb_clusters[i]):
+                    temp += "<li>"
+                    temp += " ".join(course)
+        ##  FOR TIMES       # temp += "</li></li>"
+        ##  FOR TIMES       # temp += " ".join(print_output(course))
+                    temp += "</li>"
+                temp += '''</ul>
+                            </div>'''
+                formattedblocks += temp
 
-        return '''
+            return '''
+                <html>
+                <link rel="shortcut icon" href="/static/favicon.ico">
+                <head>
+                    <link rel="stylesheet" href="/static/style.css">
+                </head>
+
+                <body>
+                    <div id="div-outermost">
+                        <div id="space_above"></div>
+                        <div id="div-flex">
+                            <h1 id="combtitle"><i class="highlight">{totalsch}</i> total schedules!</h1>
+                            <p class="titledesc">and <i class="highlight2">{coursecomb}</i> course combinations...</p>
+
+                            {blocks}
+
+                            <p><a href="/">Click here to make a new schedule</a>
+                        </div>
+                    </div>
+                </body>
+
+                </html>
+            '''.format(totalsch=allcomblength, coursecomb=len(comb_no_repeat), blocks=formattedblocks)
+        else:
+            return '''
             <html>
             <link rel="shortcut icon" href="/static/favicon.ico">
             <head>
@@ -51,21 +80,46 @@ def adder_page():
             </head>
 
             <body>
+                {errors}
                 <div id="div-outermost">
                     <div id="space_above"></div>
                     <div id="div-flex">
-                        <h1 id="combtitle"><i class="highlight">{totalsch}</i> total schedules!</h1>
-                        <p class="titledesc">and <i class="highlight2">{coursecomb}</i> course combinations...</p>
+                        <h1><i class="highlight">UN</i>CONFLICT</h1>
+                        <p id="subtitle">course combinations at Grinnell</p>
+                        <form method="post" action=".">
+                            <p class="ins">Desired Classes <i id="smallins">(should be separated by space)</i></p>
+                            <div class="form__group">
+                                <input name="course_input" class="form__input" placeholder="CSC-151 PHI-111 MAT-215" required=""/>
+                            </div>
 
-                        {blocks}
+                            <p class="ins">Number of Classes in Schedule</p>
+                            <div class="form__group">
+                                <p><input name="combination_input" class="form__input" placeholder="4" required=""/></p>
+                            </div>
 
-                        <p><a href="/">Click here to make a new schedule</a>
+
+                            <p class="ins">Must Include Sections</p>
+                            <div class="form__group">
+                                <p><input name="include_input" class="form__input" placeholder="CSC-151-01 MAT-215-03"/></p>
+                            </div>
+
+                            <p class="ins">Must Exclude Sections</p>
+                            <div class="form__group">
+                                <p><input name="exclude_input" class="form__input" placeholder="PHI-111-01"/></p>
+                            </div>
+
+                            <p><input id="submitbutton" type="submit" value="MAKE SCHEDULE"/></p>
+                            <br>
+                            <br>
+                            <p>Developed by CS Students at Grinnell College</p>
+                            <a href="/aboutus">About Us</a>
+                        </form>
                     </div>
                 </div>
             </body>
 
             </html>
-        '''.format(totalsch=allcomblength, coursecomb=len(comb_no_repeat), blocks=formattedblocks)
+            '''.format(errors=errors)
 
     return '''
     <html>
@@ -89,7 +143,7 @@ def adder_page():
 
                     <p class="ins">Number of Classes in Schedule</p>
                     <div class="form__group">
-                        <p><input name="combination_input" class="form__input" placeholder="4"/></p>
+                        <p><input name="combination_input" class="form__input" placeholder="4" required=""/></p>
                     </div>
 
 
